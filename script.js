8d30c1dd526848f92e9445c40c94b72a44d0b996
prashanttr18 <dhakalprashant278@gmail.com>
2025-08-19 07:05:10 +0545

    COMMAND ADDED

---
+++ script.js
@@ -0,0 +1,78 @@
+$ history
+    1  git --version
+    2  code .
+    3  cd ..
+    4  cd sarmila
+    5  touch index.html script.js style.css
+    6  nano readme.md
+    7  cat readme.md
+    8  cat index.html
+    9   mkdir images
+   10  clear
+   11  pwd
+   12  ls
+   13  mkdir .note
+   14  ls
+   15  la
+   16  ls -lr
+   17  ls -lR
+   18  touch random.txt
+   19  ls
+   20  rm random.txt
+   21  ls
+   22  ls
+   23  mkdir random
+   24  ls
+   25  rm random/
+   26  rm -rf random/
+   27  clear
+   28  ls
+   29  git init
+   30  ls
+   31  ls -la
+   32  git status
+   33  git add index.html
+   34  git status
+   35  git commit -m "index file initial setup"
+   36  git config user.email
+   37  git status
+   38  git status
+   39  git diff
+   40  git config user.email "dhakalprashant278@gmail.com"
+   41  git config user.name "prashanttr18"
+   42  git commit -m "index file initial setup"
+   43  git status
+   44  git log
+   45  git add .
+   46  git status
+   47  git status
+   48  git reset style.css
+   49  git status
+   50  git commit -m "all files added"
+   51  git status
+   52  clear
+   53  git status
+   54  git diff
+   55  git add index.html
+   56  git commit -m "navbar design"
+   57  git log
+   58  git remote add origin https://github.com/prashanttr18/git_learn.git
+   59  git remote add origin https://github.com/prashanttr18/git_learn.git
+   60  git push -u origin master
+   61  CLERA
+   62  clear
+   63  git branch develop
+   64  git branch
+   65  git checkout develop
+   66  git status
+   67  git add style.css
+   68  git commit -m "style file added"
+   69  git stauts
+   70  git push
+   71  git push -u
+   72  git push -u origin master
+   73  git checkout master
+   74  git merge develop
+   75  git push
+   76  git history
+   77  history
